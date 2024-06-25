1882c5f68e26ccd1f359303f3dc21510c75d5f27
Abdullah Jawed <jawed@aptech.com>
2024-06-25 20:32:41 +0500

    image update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+
+
+import { useState } from 'react';
 import './App.css';
 
+var num= 0;
+
 function App() {
+   
+  const [text , setext] = useState("abc");
+  const [result , setResult] = useState(0);
+  const [name , setName] = useState("");
+
+
+function add(){
+
+  //result = num++;
+  setResult(num++);
+
+}
+function getvalue(event){
+          console.log(event.target.value)
+          setName(event.target.value)
+}
+// function show(){}
+
+function changetext(){
+  setext(name);
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+    <input   onChange={getvalue} /> 
+      <button onClick={changetext}> change value</button>
+{text}
+
+      </div>
+    </>
+
   );
 }
 
